c14b7c1e73e53265dc0e0ef8db4823e9e99efa78
Bastián Pezoa <bpezoav@dcc.uchile.cl>
2022-11-18 11:30:11 -0300

    Version casi casi
    Se cae con caminos de largo 3. Buscar hacer consultas en un grafo mas pequeño o ponerle LIMIT a las consultas

--- parseToNt.py
+++ parseToNt.py
@@ -12,20 +12,37 @@ resultado = [{'n0': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q11
 'n1': {'xml:lang': 'ca', 'type': 'literal', 'value': 'actor australià'},
 'e2': {'type': 'uri', 'value': 'http://schema.org/description'}}]
 
-#to_parse = ['wd:22','?e0','?n0','?e1','wd:33']
-#print({to_parse[index*2 +1] : to_parse[index*2:(index*2)+3] for index in range(2)})
 
-dict = {'?e1': ['wd:22','?e0','?n0'], '?e2': ['?n0','?e1','wd:33']}
-# reccorrer el diccionario resultado
-for row in resultado:
-  #print(row)
-  for key in row.keys():
-    if (dict.get('?'+key)):
-      try:
-        v0 = dict['?'+key][0] if (':' in dict['?'+key][0]) else row[dict['?'+key][0].replace('?','')]['value']
-        v1 = dict['?'+key][1] if (':' in dict['?'+key][1]) else row[dict['?'+key][1].replace('?','')]['value']
-        v2 = dict['?'+key][2] if (':' in dict['?'+key][2]) else row[dict['?'+key][2].replace('?','')]['value']
-        filas.append([v0,v1,v2])
-      except:
-        pass
-print(filas)
+
+aristas_dict = {'?e1': ['wd:22','?e0','?n0'], '?e2': ['?n0','?e1','wd:33']}
+to_parse = ['wd:22','?e0','?n0','?e1','wd:33']
+# reccorrer el aristas_dict resultado
+def parse_to_nt(resultado, to_parse):
+  aristas_dict = {}
+  for row_arista in to_parse:
+    for index in range((len(row_arista) - 1) // 2):
+      aristas_dict[row_arista[index*2 +1]] = row_arista[index*2:(index*2)+3]
+  #aristas_dict = {to_parse[index*2 +1] : to_parse[index*2:(index*2)+3] for index in range(2)}
+  print("DICCIONARIO: ", aristas_dict, '\n')
+  for row in resultado:
+    #print(row)
+    for key in row.keys():
+      if (aristas_dict.get('?'+key)):
+        try:
+          v0 = aristas_dict['?'+key][0] if (':' in aristas_dict['?'+key][0]) else row[aristas_dict['?'+key][0].replace('?','')]['value']
+          v1 = aristas_dict['?'+key][1] if (':' in aristas_dict['?'+key][1]) else row[aristas_dict['?'+key][1].replace('?','')]['value']
+          v2 = aristas_dict['?'+key][2] if (':' in aristas_dict['?'+key][2]) else row[aristas_dict['?'+key][2].replace('?','')]['value']
+          if (('wd:' in v0) or ('wikidata.org/entity' in v0)) and (('wd:' in v1) or ('wikidata.org/prop/direct' in v1)) and (('wd:' in v2) or ('wikidata.org/entity' in v2)):
+            filas.append([v0,v1,v2])
+        except:
+          pass
+  #print(filas)
+  return filas
+
+def to_nt_file(filas):
+
+  with open('output.nt', 'w') as f:
+    for fila in filas:
+      v0 = fila[0].replace('wd:', 'http://www.wikidata.org/entity/')
+      v2 = fila[2].replace('wd:', 'http://www.wikidata.org/entity/')
+      f.write('<{}> <{}> <{}> .\n'.format(v0, fila[1], v2))
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import main
+import parseToNt
 
 entidades = ["m1","m2"]
 
@@ -10,6 +12,7 @@ def get_query(entidades,largo_camino):
 	nodo_num = 0
 	arista_num = 0
 	querys = []
+	lista_aristas = []
 	x = 0
 	while x< (2**largo_camino):
 
@@ -37,16 +40,25 @@ def get_query(entidades,largo_camino):
 		querys.append(act_query)
 		#print(act_query)
 		x+=1
+		lista_aristas.append(aristas)
 
 	big_query = 'UNION\n'.join(querys)
 	big_query = 'SELECT DISTINCT *\nWHERE{\n' + big_query + '}'
 	#print(big_query)
-	return big_query
+	return big_query, lista_aristas
 
 if __name__ == '__main__':
-	entidad_1 = input("Primera entidad (ej wd:214): ")
-	entidad_2 = input("Segunda entidad (ej wd:214): ")
+	#entidad_1 = input("Primera entidad (ej wd:214): ")
+	entidad_1 = 'wd:Q180553'
+	#entidad_2 = input("Segunda entidad (ej wd:214): ")
+	entidad_2 = 'wd:Q12393'
 	largo_camino = int(input("Largo máximo de los caminos (ej 3): "))
 	print("\n\n")
-	print(get_query([entidad_1, entidad_2],largo_camino))
-
+	#print(get_query([entidad_1, entidad_2],largo_camino))
+	endpoint_url = "https://query.wikidata.org/sparql"
+	query, aristas = get_query([entidad_1, entidad_2],largo_camino)
+	result = main.get_results(endpoint_url, query)
+	print("RESULTS BINDINGS:", result["results"]["bindings"], '\n')
+	print("ARISTAS:", aristas, '\n')
+	parsed_to_nt = parseToNt.parse_to_nt(result["results"]["bindings"], aristas)
+	parseToNt.to_nt_file(parsed_to_nt)
